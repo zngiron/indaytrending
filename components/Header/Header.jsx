073ae6731fd55f50ad708b6bf29b16e82f9cafdb
Zuen Giron <zngiron@ZnGiron-MacBook-Pro.local>
2020-03-21 14:49:04 +0800

    Removed: Header GraphQL Fetch

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,24 +1,15 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { useSpring } from 'react-spring';
-import { useQuery } from '@apollo/react-hooks';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import * as UI from './Header.styled';
+import categories from './Header.json';
+
 import Overlay from '../Overlay';
-
-import { GET_CATEGORIES } from '../../library/Queries.graphql';
-
-const handleIcon = ({ loading, error, menu }) => {
-  if (loading) return 'spinner-third';
-  if (error) return 'exclamation-circle';
-
-  return menu ? 'times' : 'bars';
-};
 
 const Header = () => {
   const [menu, setMenu] = useState(false);
-  const { loading, error, data } = useQuery(GET_CATEGORIES);
 
   const springIcon = useSpring({ transform: menu ? 'rotate(-180deg)' : 'rotate(0deg)' });
   const springMenu = useSpring({ transform: menu ? 'translate3d(-20rem, 0, 0)' : 'translate3d(0rem, 0 ,0)' });
@@ -46,20 +37,18 @@ const Header = () => {
         <UI.Nav>
           <UI.Button aria-label="menu" onClick={() => handleMenu({ toggle: true })}>
             <UI.Icon style={springIcon}>
-              <FontAwesomeIcon icon={['far', handleIcon({ loading, error, menu })]} spin={loading} />
+              <FontAwesomeIcon icon={['far', menu ? 'times' : 'bars']} />
             </UI.Icon>
           </UI.Button>
-          {data && (
-            <UI.Menu style={springMenu}>
-              {data.categories.nodes.map((category) => (
-                <UI.List key={category.id}>
-                  <Link href="/[category]" as={`/${category.slug}`} passHref>
-                    <UI.Item onClick={() => handleMenu()}>{category.name}</UI.Item>
-                  </Link>
-                </UI.List>
-              ))}
-            </UI.Menu>
-          )}
+          <UI.Menu style={springMenu}>
+            {categories.nodes.map((category) => (
+              <UI.List key={category.id}>
+                <Link href="/[category]" as={`/${category.slug}`} passHref>
+                  <UI.Item onClick={() => handleMenu()}>{category.name}</UI.Item>
+                </Link>
+              </UI.List>
+            ))}
+          </UI.Menu>
         </UI.Nav>
       </UI.Header>
     </>
