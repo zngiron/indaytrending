665979943a1776025ed1bb58fbaa98a054c5669c
Zuen Giron <zngiron@ZnGiron-MacBook-Pro.local>
2020-03-25 12:41:18 +0800

    Fixed: Trim

--- library/Functions.js
+++ library/Functions.js
@@ -1,7 +1,7 @@
 import sanitize from 'sanitize-html';
-import { oneLineTrim } from 'common-tags';
+import { oneLine } from 'common-tags';
 
-export const clean = (html) => oneLineTrim(
+export const clean = (html) => oneLine(
   sanitize(html, {
     allowedTags: ['h2', 'h3', 'h4', 'h5', 'h6', 'p', 'strong', 'img'],
     allowedAttributes: {
