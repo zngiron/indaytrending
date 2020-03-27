5c8210fd945d825463ce531ccc44be79cd80414a
Zuen Giron <zngiron@ZnGiron-MacBook-Pro.local>
2020-03-28 00:24:40 +0800

    Fixed: Adsense

--- components/Ads/Adsense/Adsense.jsx
+++ components/Ads/Adsense/Adsense.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 
 import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const Adsense = ({
   slot,
@@ -8,22 +9,26 @@ const Adsense = ({
   format = 'auto',
   responsive = true,
 }) => {
+  const { asPath } = useRouter();
+
   useEffect(() => {
     if (window.adsbygoogle) {
       (adsbygoogle = window.adsbygoogle || []).push({});
     }
-  }, []);
+  }, [asPath]);
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: 'block', margin: '1.25rem 0', textAlign: 'center' }}
-      data-ad-client="ca-pub-9878085739428147"
-      data-ad-slot={slot}
-      data-ad-layout={layout || ''}
-      data-ad-format={format}
-      data-full-width-responsive={responsive}
-    />
+    <div key={asPath} style={{ textAlign: 'center' }}>
+      <ins
+        className="adsbygoogle"
+        style={{ display: 'block', margin: '1.25rem 0', textAlign: 'center' }}
+        data-ad-client="ca-pub-9878085739428147"
+        data-ad-slot={slot}
+        data-ad-layout={layout || ''}
+        data-ad-format={format}
+        data-full-width-responsive={responsive}
+      />
+    </div>
   );
 };
 
