6a665a92fc048643a4c0a180f31ec0d6346ccb1b
Zuen Giron <zngiron@ZnGiron-MacBook-Pro.local>
2020-04-05 16:07:38 +0800

    Fixed: Taboola

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 
 import * as Layout from '../UI/Layout.styled';
 
 const Header = dynamic(import('../Header'));
 const Footer = dynamic(import('../Footer'));
+const Taboola = dynamic(import('../Ads/Taboola'));
 
-const Root = ({ children }) => (
-  <>
-    <Layout.Global styles={Layout.Styles} />
-    <Header />
-    <Layout.Main>{children}</Layout.Main>
-    <Footer />
-  </>
-);
+const Root = ({ children }) => {
+  const { query } = useRouter();
+
+  return (
+    <>
+      <Layout.Global styles={Layout.Styles} />
+      <Header />
+      <Layout.Main>{children}</Layout.Main>
+      <Layout.Container hidden={query.slug === undefined}>
+        <Taboola />
+      </Layout.Container>
+      <Footer />
+    </>
+  );
+};
+
 export default Root;
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -31,10 +31,6 @@ export default class Root extends Document {
             __html: '_atrk_opts={atrk_acct:"4FMtu1Y1Mn20Io",domain:"indaytrending.com",dynamic:!0},function(){const t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://certify-js.alexametrics.com/atrk.js";const e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}();',
           }}
           />
-          <script dangerouslySetInnerHTML={{
-            __html: 'window._taboola=window._taboola||[],_taboola.push({article:"auto"}),function(e,o,t,a){document.getElementById(a)||(e.async=1,e.src="//cdn.taboola.com/libtrc/indaytradingsc/loader.js",e.id=a,o.parentNode.insertBefore(e,o))}(document.createElement("script"),document.getElementsByTagName("script")[0],0,"tb_loader_script"),window.performance&&"function"==typeof window.performance.mark&&window.performance.mark("tbl_ic");',
-          }}
-          />
           <noscript><img src="https://certify.alexametrics.com/atrk.gif?account=4FMtu1Y1Mn20Io" width={1} height={1} alt="" style={{ display: 'none' }} /></noscript>
         </body>
       </Html>
