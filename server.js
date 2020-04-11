c7eba360a6849b48cf744aff9d772f43360fa6fb
Zuen Giron <zngiron@ZnGiron-MacBook-Pro.local>
2020-04-11 17:30:07 +0800

    Added: Redis

--- server.js
+++ server.js
@@ -3,41 +3,34 @@
 const Express = require('express');
 const Helmet = require('helmet');
 const Next = require('next');
-const LRUCache = require('lru-cache');
+const Redis = require('redis');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const development = process.env.NODE_ENV !== 'production';
+const client = Redis.createClient();
 const app = Next({ development });
 const handle = app.getRequestHandler();
 
-const cache = new LRUCache({
-  max: 100 * 1024 * 1024,
-  maxAge: 1000 * 60 * 60,
-});
+const handleCache = (req, res) => {
+  const key = `${req.path}`;
 
-const getCacheKey = (req) => `${req.path}`;
-
-const handleCache = async (req, res) => {
-  const key = getCacheKey(req);
-
-  if (cache.has(key)) {
-    res.setHeader('X-Cache', 'HIT');
-    return res.send(cache.get(key));
-  }
-
-  try {
-    const html = await app.renderToHTML(req, res, req.path, req.query);
-
-    if (res.statusCode !== 200) {
-      return res.send(html);
+  client.get(key, async (error, cache) => {
+    if (cache !== null) {
+      res.setHeader('X-Cache', 'HIT');
+      return res.send(cache);
     }
 
-    cache.set(key, html);
-    res.setHeader('X-Cache', 'MISS');
-    return res.send(html);
-  } catch (error) {
-    return app.renderError(error, req, res, req.path, req.query);
-  }
+    try {
+      const html = await app.renderToHTML(req, res, req.path, req.query);
+
+      client.set(key, html);
+
+      res.setHeader('X-Cache', 'MISS');
+      return res.send(html);
+    } catch (err) {
+      app.renderError(err, req, res, req.path, req.query);
+    }
+  });
 };
 
 app.prepare().then(() => {
