75fbff6b3af19b3e58292630a31fc6d05ee56bfb
Zuen Giron <zngiron@ZnGiron-MacBook-Pro.local>
2020-05-19 00:19:34 +0800

    Added: Alexa

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,6 +8,8 @@ import Document, {
   NextScript,
 } from 'next/document';
 
+import { oneLineTrim } from 'common-tags';
+
 export default class Root extends Document {
   render() {
     return (
@@ -20,6 +22,13 @@ export default class Root extends Document {
           <script async src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v6.0&amp;appId=1201824889948708&amp;autoLogAppEvents=1" />
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
           <script async src="https://cdn.taboola.com/libtrc/indaytradingsc/loader.js" id="tb_loader_script" />
+          <script dangerouslySetInnerHTML={{
+            __html: oneLineTrim`_atrk_opts={atrk_acct:"4FMtu1Y1Mn20Io",domain:"indaytrending.com",dynamic:!0},function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://certify-js.alexametrics.com/atrk.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}();`,
+          }}
+          />
+          <noscript>
+            <img src="https://certify.alexametrics.com/atrk.gif?account=4FMtu1Y1Mn20Io" height="1" width="1" alt="" style={{ display: 'none' }} />
+          </noscript>
         </body>
       </Html>
     );
