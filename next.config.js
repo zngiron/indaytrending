62f14423ca0a42df3bc5f0abff634c674e7c7b89
Zuen Giron <zgiron@FBCLM-ZGIRON.local>
2020-06-19 17:09:00 +0800

    Added: Service Worker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,40 @@
-module.exports = {
+const path = require('path');
+const nextOffline = require('next-offline');
+
+const nextConfig = {
+  workboxOpts: {
+    swDest: path.resolve('public', 'service-worker.js'),
+    runtimeCaching: [
+      {
+        urlPattern: /https?:\/\/(cms.)?indaytrending.com\/.*\.(jpg|png|gif|svg|webp)$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'images',
+          expiration: {
+            maxEntries: 200,
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+          },
+        },
+      },
+      {
+        urlPattern: /\.json$/,
+        handler: 'CacheFirst',
+        options: {
+          cacheName: 'stories',
+        },
+      },
+      {
+        urlPattern: /^https?:\/\/(cms.)?indaytrending.com.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'cache',
+        },
+      },
+    ],
+  },
   env: {
     DOMAIN: process.env.DOMAIN,
   },
 };
+
+module.exports = nextOffline(nextConfig);
