ec79f79441fa6fdfe814502a0cebe3c206fc22aa
Zuen Giron <zngiron@ZnGiron-MacBook-Pro.local>
2020-07-13 20:56:09 +0800

    Fixed: Pagination

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 import styled from '@emotion/styled';
-import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import * as Global from './Global';
-import * as Button from './Button';
 
 const Content = dynamic(import('./Content'));
 const Card = dynamic(import('./Card'));
+const Pagination = dynamic(import('./Pagination'));
 
 const Root = styled.div`
   padding: 2.5rem 0;
@@ -27,56 +25,17 @@ const Container = styled(Global.Container)`
   }
 `;
 
-const Pagination = styled(Global.Container)`
-  display: flex;
-  margin-top: 1.25rem;
-
-  ${Button.Primary} {
-    margin-right: 0.5rem;
-  }
-`;
-
-const Text = styled.span`
-  margin: 0 0.5rem;
-`;
-
 const Stories = ({ posts, category }) => (
   <Root>
     <Content
       title={category?.name}
       description={`</p>${category?.description}</p>`}
     />
+    <Pagination posts={posts} category={category} />
     <Container>
       {posts?.edges?.map(({ node }) => <Card key={node.id} post={node} />)}
     </Container>
-    <Pagination>
-      {posts.pageInfo.hasPreviousPage && (
-      <Link
-        href="/page"
-        as={`/page?category=${category.slug}&before=${posts.pageInfo.startCursor}`}
-        prefetch={false}
-        passHref
-      >
-        <Button.Primary>
-          <FontAwesomeIcon icon={['far', 'chevron-left']} />
-          <Text>Previous</Text>
-        </Button.Primary>
-      </Link>
-      )}
-      {posts.pageInfo.hasNextPage && (
-      <Link
-        href="/page"
-        as={`/page?category=${category.slug}&after=${posts.pageInfo.endCursor}`}
-        prefetch={false}
-        passHref
-      >
-        <Button.Primary>
-          <Text>Next</Text>
-          <FontAwesomeIcon icon={['far', 'chevron-right']} />
-        </Button.Primary>
-      </Link>
-      )}
-    </Pagination>
+    <Pagination posts={posts} category={category} />
   </Root>
 );
 
