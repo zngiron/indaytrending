8a8078af725bb39847e8bc2c7a1161525be1efed
Zuen Giron <zgiron@FBCLM-ZGIRON.local>
2020-07-14 15:37:25 +0800

    Updated: Feed Image

--- pages/api/feed.js
+++ pages/api/feed.js
@@ -17,7 +17,7 @@ const Story = ({ post }) => html`
       <article>
         <header>
           <figure>
-            <img src="${post.image ? `${process.env.DOMAIN}/api/image?url=${post?.image?.featured}` : `${process.env.DOMAIN}/indaytrending-thumbnail.png`}" alt="${post?.title}" />
+            <img src="${post?.image?.featured || `${process.env.DOMAIN}/indaytrending-thumbnail.png`}" alt="${post?.title}" />
           </figure>
           <time class="op-published" datetime="${date(post?.published)}">${date(post?.published)}</time>
           <time class="op-modified" datetime="${date(post?.modified)}">${date(post?.modified)}</time>
