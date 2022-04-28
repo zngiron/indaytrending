b0e38a815610c403f0135990d69e9517f5165d32
Zuen Giron <zngiron@yahoo.com>
2022-04-29 00:56:52 +0800

    Updated: AdSense Code

--- components/Adsense.jsx
+++ components/Adsense.jsx
@@ -1,34 +1,36 @@
+/* eslint-disable no-undef */
+
+import { useEffect } from 'react';
+
 function Adsense({ type, slot }) {
+  useEffect(() => {
+    setTimeout(() => {
+      if (window.adsbygoogle) {
+        (adsbygoogle = window.adsbygoogle || []).push({});
+      }
+    }, 500);
+  }, []);
+
   if (type === 'article') {
     return (
-      <>
-        <ins
-          className="adsbygoogle block text-center"
-          data-ad-layout="in-article"
-          data-ad-format="fluid"
-          data-ad-client="ca-pub-9878085739428147"
-          data-ad-slot={slot}
-        />
-        <script>
-          (adsbygoogle = window.adsbygoogle || []).push();
-        </script>
-      </>
+      <ins
+        className="adsbygoogle block text-center"
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-9878085739428147"
+        data-ad-slot={slot}
+      />
     );
   }
 
   return (
-    <>
-      <ins
-        className="adsbygoogle block"
-        data-ad-client="ca-pub-9878085739428147"
-        data-ad-slot={slot}
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      />
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push();
-      </script>
-    </>
+    <ins
+      className="adsbygoogle block"
+      data-ad-client="ca-pub-9878085739428147"
+      data-ad-slot={slot}
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
   );
 }
 
