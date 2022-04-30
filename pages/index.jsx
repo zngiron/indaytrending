f6e7f40715e094d661e652950d91f9c6741e0587
Zuen Giron <zngiron@yahoo.com>
2022-04-30 08:31:16 +0800

    Updated: Optimization

--- pages/index.jsx
+++ pages/index.jsx
@@ -43,7 +43,7 @@ export async function getStaticProps() {
       categories,
       category: categories.edges.find(({ node }) => node.slug === category).node,
     },
-    revalidate: 30,
+    revalidate: 60,
   };
 }
 
