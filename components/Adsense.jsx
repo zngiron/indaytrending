bee6ed5574991e5fb7ed46a66cd950c13839d11c
Zuen Giron <zngiron@gmail.com>
2022-10-18 12:33:06 +0100

    Fixed: Adsense Ads

--- components/Adsense.jsx
+++ components/Adsense.jsx
@@ -5,13 +5,16 @@ export const handleAdsense = () => {
 
 function Adsense({ slot }) {
   return (
-    <ins
-      className="adsbygoogle adsense block mx-auto text-center"
-      data-ad-client="ca-pub-9878085739428147"
-      data-ad-slot={slot}
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    />
+    <span className="block my-5">
+      <span className="block py-5 text-sm text-center">Advertisement</span>
+      <ins
+        className="adsbygoogle adsense block mx-auto text-center"
+        data-ad-client="ca-pub-9878085739428147"
+        data-ad-slot={slot}
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+    </span>
   );
 }
 
--- library/functions.js
+++ library/functions.js
@@ -1,7 +1,7 @@
 import sanitize from 'sanitize-html';
 
 export const clean = (html) => sanitize(html, {
-  allowedTags: ['p', 'img'],
+  allowedTags: ['p', 'img', 'span'],
   allowedAttributes: {
     img: ['src', 'srcset', 'width', 'height', 'alt', 'loading'],
   },
@@ -12,13 +12,16 @@ export const date = (e) => new Date(e).toISOString();
 export const ads = (html) => {
   const re = /<.*?>.*?<\/.*?>/gms;
   const element = `
-    <ins
-      class="adsbygoogle adsense block mx-auto text-center"
-      data-ad-client="ca-pub-9878085739428147"
-      data-ad-slot="3640794162"
-      data-ad-format="fluid"
-      data-ad-layout="in-article"
-    ></ins>
+    <span class="block my-5">
+      <span class="block py-5 text-sm text-center">Advertisement</span>
+      <ins
+        class="adsbygoogle adsense block mx-auto text-center"
+        data-ad-client="ca-pub-9878085739428147"
+        data-ad-slot="3640794162"
+        data-ad-format="fluid"
+        data-ad-layout="in-article"
+      ></ins>
+    </span>
   `;
 
   const content = html.match(re)?.map((item, id) => {
