6566020a9903871b090d01a6f359d84d9b430302
Zuen Giron <zngiron@gmail.com>
2024-10-09 21:18:15 +0800

    fix(api): add fetch caching

--- src/library/api.ts
+++ src/library/api.ts
@@ -2,4 +2,11 @@ import { GraphQLClient } from 'graphql-request';
 
 import { env } from '@/library/environment';
 
-export const client = new GraphQLClient(`${env.API}/graphql`);
+export const client = new GraphQLClient(`${env.API}/graphql`, {
+  fetch: (url, options) => fetch(url, {
+    ...options,
+    next: {
+      revalidate: 60 * 60,
+    },
+  }),
+});
