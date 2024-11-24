25992462b86cfbf06ff7d64b4c2750020af8ae77
Zuen Giron <zngiron@gmail.com>
2024-11-24 15:44:47 +0800

    fix: api caching

--- src/library/api.ts
+++ src/library/api.ts
@@ -1,13 +1,16 @@
 import { GraphQLClient } from 'graphql-request';
 
+import { cache } from 'react';
+
 import { env } from '@/library/environment';
 
 export const client = new GraphQLClient(`${env.API}/graphql`, {
-  fetch: (url, options) =>
+  fetch: cache(async (url: RequestInfo | URL, options?: RequestInit) =>
     fetch(url, {
       ...options,
       next: {
         revalidate: 86400,
       },
     }),
+  ),
 });
